33def6aee99fa671c90fee606d4eacfc11f7fa5e
npv34 <phanvanluan611996@gmail.com>
2020-10-26 18:53:16 +0700

    logout

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private router: Router) { }
+
+  getUserLogin() {
+    return JSON.parse(localStorage.getItem('userLogin'));
+  }
+
+  logout() {
+    localStorage.removeItem('userLogin')
+    this.router.navigate(['login'])
+  }
 }
